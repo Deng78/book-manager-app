252220ea8e99b3a93e72d83fc966314e4d8fa9dd
Deng N <dengneyal@gmail.com>
2018-03-14 15:36:48 +1000

    added async requests to server and render server validation errors

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
 import LoginForm from '../forms/LoginForm'
+import { login } from '../../actions/auth';
 
 class LoginPage extends React.Component {
-    submit = data => {
-        console.log(data);
-    }
+    submit = data => this.props.login(data)
+    .then(() => this.props.history.push("/"));
    render(){
        return (
         <div>
@@ -14,5 +16,11 @@ class LoginPage extends React.Component {
        )
    } 
 }
+LoginPage.propTypes = {
+    history: propTypes.shape({
+      push: propTypes.func.isRequired
+    }).isRequired,
+    login: propTypes.func.isRequired
+  };
 
-export default LoginPage;
+export default connect(null, { login })(LoginPage);
